8a20baeb21435ae4f54c40a3673d6007d4668618
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-11-14 20:29:52 -0300

    create the App component structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 import firebase from "./connections/firebaseConnection";
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  function handleAdd() {
+    alert('CLICOU');
+  };
+
   return (
     <div>
-      <h1>ReactJS + Firebase :)</h1>
+      <h1>ReactJS + Firebase</h1>
+      <br />
+
+      <label>Titulo: </label>
+      <textarea type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
+
+      <label>Autor: </label>
+      <input type="text" value={author} onChange={(event) => setAuthor(event.target.value)} />
+
+      <button onClick={handleAdd} >Cadastrar</button>
+
     </div>
   );
 };
